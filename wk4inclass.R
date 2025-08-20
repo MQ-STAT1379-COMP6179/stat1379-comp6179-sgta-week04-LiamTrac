f695885e51cc7b51d506f36f674470e8ac3f6772
LiamTrac <liam.tracy@students.mq.edu.au>
2025-08-20 14:48:29 +1000

    final commit

--- wk4inclass.R
+++ wk4inclass.R
@@ -1,9 +1,11 @@
+##q3
+#3.1.1
 cholesterol_data <- tibble(
   oat_bran_free = c(4.61, 5.42, 5.40, 4.54, 3.98), 
   oat_bran = c(3.64, 5.57, 4.85, 4.80, 3.68)
 )
 cholesterol_data
-
+#3.1.2
 col_long <- cholesterol_data |>
   pivot_longer(
     cols = c(`oat_bran_free`, `oat_bran`),z
@@ -11,7 +13,7 @@ col_long <- cholesterol_data |>
     values_to = "col_lvl"
   )
 col_long
-
+#3.1.3
 col_long|>
 summarise(
   n = n_distinct(col_lvl),
